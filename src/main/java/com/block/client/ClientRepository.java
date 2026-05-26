16941b8689509af65c4b56c2405694d6430cff66
suryprakash <suryprakash@artcode.in>
2026-05-26 10:55:34 +0530

    adding client entity

---
+++ src/main/java/com/block/client/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.block.client;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long>{
+
+}
